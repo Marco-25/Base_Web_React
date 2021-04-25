c850fb8fe883b47a63c35b9269207b3827a03c4d
Marco-25 <marco.antonio-25@outlook.com.br>
2021-04-24 23:33:15 -0300

    ...

--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useTransition } from 'react-spring';
 import { ToastMessage } from '../../hooks/ToastContext';
 import { Container } from './styles';
 import Toast from './Toast';
@@ -9,15 +8,6 @@ interface ToastProps {
 }
 
 const ToastContainer: React.FC<ToastProps> = ({ messages }) => {
-  const messagesWithTransitions = useTransition(
-    messages,
-    // message => message.id,
-    {
-      from: { right: '-120%' },
-      enter: { right: '0%}' },
-      leave: { right: '-120%' },
-    }
-  );
 
   return (
     <Container>
